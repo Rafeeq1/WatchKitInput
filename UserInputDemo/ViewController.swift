ac96d19b3c2117f068fab9ed1eb83b27c0c6a5c0
eeshana <rafiq.mulla@eeshana.com>
2015-12-09 15:16:51 +0530

    Initial Commit

---
+++ UserInputDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserInputDemo
+//
+//  Created by Rafeeq on 09/12/15.
+//  Copyright © 2015 Eeshana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
